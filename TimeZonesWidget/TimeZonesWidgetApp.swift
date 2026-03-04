ee21e03a326bacb7160123504fe771f9a6a91997
Dmitry Marchenko <abubiker@icloud.com>
2026-03-04 17:39:40 +0300

    Initial Commit

---
+++ TimeZonesWidget/TimeZonesWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeZonesWidgetApp.swift
+//  TimeZonesWidget
+//
+//  Created by Дмитрий Марченко on 04.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TimeZonesWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
